845a013da15eacbc0bd33d2add42c2efc1797823
unknown <ctec66@gmail.com>
2017-09-18 17:32:25 -0400

    Cleaned up the code a bit

--- marriage.py
+++ marriage.py
@@ -1,42 +1,40 @@
 # Chris Swanson and Chris Casey - CSC 440 - September 20, 2017
 import sys
 
+# Global Variables
 knights = {}
 ladies = {}
-people = {}
-
-knights1 = {
-    'Galahad': ['Guinevere', 'Elaine'],
-    'Lancelot': ['Guinevere', 'Elaine']}
-ladies1 = {
-    'Guinevere': ['Galahad', 'Lancelot'],
-    'Elaine': ['Lancelot', 'Galahad']}
-matches = []
+matches = {}
 numPairs = 0
 
+# Reads in the given data file line by line
 def read_file(filename):
+    i = 0
     with open(filename, 'r') as f:
-        file = []
-
         # gets the first value of the file so that we know how many matches we need to make
         # this is a string value at the moment
         numPairs = f.readline()
+        j = int(numPairs)
 
         # reads each of the other lines of the dictionary
         for line in f:
-            items = line.split()
-            key, values = items[0], items[1:]
-            people[key] = values
-        print("Number of pairs: ", numPairs)
-        print(people, end='')
+            # splits the knights and ladies into separate dictionaries
+            if (i < j):
+                items = line.split()
+                key, values = items[0], items[1:]
+                knights[key] = values
+                i = i + 1
+            else:
+                items = line.split()
+                key, values = items[0], items[1:]
+                ladies[key] = values
 
+# def perfect_matches():
+
+
+# Prints out the matches in the specified format
 def printPairings():
-    print("Functionality to be added")
-
-def perfect_matches():
-    for key, value in knights1.items():
-        matches.append(key)
-        matches.append(value)
+        print("Functionality to be added")
 
 def main():
     read_file('ten.txt')
