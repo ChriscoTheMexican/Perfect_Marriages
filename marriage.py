2e38e1001bb1d1794b1660e0a1551febfdcd212c
unknown <ctec66@gmail.com>
2017-09-19 16:14:31 -0400

    Matches are working, code cleaned up, comments added

--- marriage.py
+++ marriage.py
@@ -1,13 +1,12 @@
 # Chris Swanson and Chris Casey - CSC 440 - September 20, 2017
 import sys
+import timeit
 
-# Global Variables
 knights = {}
 ladies = {}
-matches = {}
+engaged = {}
 numPairs = 0
 
-# Reads in the given data file line by line
 def read_file(filename):
     i = 0
     with open(filename, 'r') as f:
@@ -18,7 +17,6 @@ def read_file(filename):
 
         # reads each of the other lines of the dictionary
         for line in f:
-            # splits the knights and ladies into separate dictionaries
             if (i < j):
                 items = line.split()
                 key, values = items[0], items[1:]
@@ -29,15 +27,54 @@ def read_file(filename):
                 key, values = items[0], items[1:]
                 ladies[key] = values
 
-# def perfect_matches():
+def perfect_matches():
+    # gets the next knight to be matched
+    knightsToBeMatched = [item[0:] for item in knights]
 
+    while knightsToBeMatched:
+        # pops the first knight from the list
+        current_knight = knightsToBeMatched.pop(0)
 
-# Prints out the matches in the specified format
-def printPairings():
-        print("Functionality to be added")
+        # Gets the current knights choice for a lady
+        engagement_choices = knights[current_knight]
+
+        # gets the ladies next peferance for a knight
+        current_lady = engagement_choices.pop(0)
+
+        # matches current
+        match = engaged.get(current_lady)
+
+        # Checks to see if the lady is engaged
+        if not match:
+            engaged[current_lady] = current_knight
+        else:
+            # gets the current lady that needs to be matches
+            ladiesToBeMatched = ladies[current_lady]
+
+            if ladiesToBeMatched.index(match) > ladiesToBeMatched.index(current_knight):
+                # The lady has found a better match
+                engaged[current_lady] = current_knight
+
+                if knights[match]:
+                    knightsToBeMatched.append(match)
+            else:
+                if engagement_choices:
+                    # Look again
+                    knightsToBeMatched.append(current_knight)
+    return engaged
+
+def print_pairings():
+    # Prints out each match on its own line with the knight first and then
+    for key, value in engaged.items():
+        print(value, key)
 
 def main():
     read_file('ten.txt')
+
+    # Finds the matches for each knight and lady
     perfect_matches()
+
+    # prints the pairings of knights and ladies
+    print_pairings()
 
 main()
